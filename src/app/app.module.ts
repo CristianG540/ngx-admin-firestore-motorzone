d198b463605a2760e7e7342554ac2c8e3376bcb6
CristianG540 <cristianneatproy@gmail.com>
2018-08-09 11:32:19 -0500

    correcion estandar codigo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,25 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-import { APP_BASE_HREF } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { CoreModule } from './@core/core.module';
-import { HttpClientModule } from '@angular/common/http'; // angular >5
+import { APP_BASE_HREF } from '@angular/common'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NgModule } from '@angular/core'
+import { HttpModule } from '@angular/http'
+import { CoreModule } from './@core/core.module'
+import { HttpClientModule } from '@angular/common/http' // angular >5
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ThemeModule } from './@theme/theme.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NB_AUTH_TOKEN_WRAPPER_TOKEN, NbAuthJWTToken } from '@nebular/auth';
+import { AppComponent } from './app.component'
+import { AppRoutingModule } from './app-routing.module'
+import { ThemeModule } from './@theme/theme.module'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { NB_AUTH_TOKEN_WRAPPER_TOKEN, NbAuthJWTToken } from '@nebular/auth'
 
-import { AuthGuard } from './auth-guard.service';
+import { AuthGuard } from './auth-guard.service'
 
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireModule } from 'angularfire2'
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database'
+import { AngularFireAuthModule } from 'angularfire2/auth'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDvspmoR6kHwKFxabc2tMgMG9myJdwizNY',
@@ -29,8 +29,8 @@ const firebaseConfig = {
   databaseURL: 'https://firestore-test-1-todo.firebaseio.com',
   projectId: 'firestore-test-1-todo',
   storageBucket: 'firestore-test-1-todo.appspot.com',
-  messagingSenderId: '227096854617',
-};
+  messagingSenderId: '227096854617'
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -45,15 +45,15 @@ const firebaseConfig = {
     AngularFireAuthModule,
     NgbModule.forRoot(),
     ThemeModule.forRoot(),
-    CoreModule.forRoot(),
+    CoreModule.forRoot()
   ],
   bootstrap: [AppComponent],
   providers: [
     { provide: APP_BASE_HREF, useValue: '/' },
     { provide: NB_AUTH_TOKEN_WRAPPER_TOKEN, useClass: NbAuthJWTToken },
     AuthGuard,
-    AngularFireDatabase,
-  ],
+    AngularFireDatabase
+  ]
 })
 export class AppModule {
 }
